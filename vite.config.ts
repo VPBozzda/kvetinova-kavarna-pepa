f0c0ec22e279331c8918c26c3fed191f0a47f26d
VPBozzda <vojta.z.vesnice@gmail.com>
2026-06-11 01:28:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
-import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [
-    nitro({
-      preset: "vercel",
-    }),
-  ],
+  nitro: true,
 });
